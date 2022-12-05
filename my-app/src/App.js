c37a09afba79a254d60858c722ea4915c9070912
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-05 17:55:55 -0300

    Replaced navbar with a layout containing footer and navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,12 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
+import LayOut from "./components/LayOut";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<NavBar />}>
+      <Route path="/" element={<LayOut />}>
         <Route index element={<Home />} />
         <Route path="about" element={<AboutUs />} />
         <Route path="contact-us" element={<ContactUs />} />
