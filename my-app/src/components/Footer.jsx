9e8d43e0ef127eda0ab6b009f8d10ad404c61d58
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-05 17:56:06 -0300

    Added footer

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        width: "100%",
+        height: "100px",
+      }}
+    >
+      <h2 style={{ color: "white", textAlign: "center" }}>Footer</h2>
+    </div>
+  );
+};
+
+export default Footer;
