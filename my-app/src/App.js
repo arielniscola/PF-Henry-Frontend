73ad215903ccba17688802ffcd20fd105eba5484
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-05 16:11:38 -0300

    Added app routing. AboutUs/ContactUs/Home/Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<AboutUs />} />
+        <Route path="contact-us" element={<ContactUs />} />
+      </Route>
+    </Routes>
   );
 }
 
 export default App;
+
