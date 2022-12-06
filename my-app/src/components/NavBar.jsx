8f0d1685481ef57d0034a00709e3e57935521ecc
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-06 16:39:16 -0300

    masomenos estilizado

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -3,32 +3,24 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <>
-      <nav
-        className="navigation-container"
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "100%",
-          height: "70px",
-        }}
-      >
-        <div className="logo">
-          <Link to="/">Logo</Link>
+      <nav className="shadow-2xl flex justify-between w-auto m-auto rounded-xl p-10 h-16  mx-5 items-center">
+        <div className="">
+          <Link to="/">
+            <p className="text-2xl">
+              PF<span className="font-bold"> Henry</span>
+            </p>
+          </Link>
         </div>
 
-        <div
-          className="nav-links"
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "50%",
-          }}
-        >
+        <div className="hidden md:flex gap-10">
           <Link to="about">About us</Link>
           <Link to="contact-us">Contact us</Link>
+          <Link to="pricing">Pricing</Link>
         </div>
 
-        <div className="login">Profile/Login</div>
+        <button className="bg-green-600 md:px-12 rounded-2xl py-2  text-white font-bold md:text-xl xs: text-xs xs: px-5 ">
+          Log in
+        </button>
       </nav>
     </>
   );
