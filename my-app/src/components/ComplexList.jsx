25172df5e047e2f8b84b7c8052ab48f88dd3c5a1
AlexisAraujo530 <alexisaraujo530@gmail.com>
2022-12-14 16:52:03 -0300

    Deploy

--- my-app/src/components/ComplexList.jsx
+++ my-app/src/components/ComplexList.jsx
@@ -1,16 +1,13 @@
 import { complexs } from "../data/complexsExample";
-
 import ComplexCard from "./ComplexCard";
 
 const ComplexList = () => {
   return (
-    <div className="max-h-[600px] w-full m-auto shadow-xl flex my-5 py-5 ">
-      <div className="flex  flex-col items-start h-auto w-1/2 overflow-y-scroll scrollbar">
+
+      <div className="flex  flex-row items-start  overflow-y-scroll scrollbar">
         {complexs &&
           complexs.map((complex,index) => <ComplexCard  key={index} complexDetails={complex} />)}
       </div>
-      <div className="">Mapa</div>
-    </div>
   );
 };
 
