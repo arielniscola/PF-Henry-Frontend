6c605167b2000290714d09152b7cf88ffb913311
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-15 21:09:42 -0300

    Added user login on app mount

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,9 +8,21 @@ import Error404 from "./components/Error404";
 import Account from "./components/Account";
 import ComplexDetails from "./components/ComplexDetails";
 import ComplexContainer from "./components/complexContainer";
-
+import Register from "./components/Register";
+import Login from "./components/Login";
+import ForgotPassword from "./components/ForgotPassword";
+import NewPassword from "./components/NewPassword";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { checkUserSession } from "./redux/actions";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkUserSession());
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<LayOut />}>
@@ -18,8 +30,12 @@ function App() {
         <Route path="about" element={<AboutUs />} />
         <Route path="contact-us" element={<ContactUs />} />
         <Route path="account" element={<Account />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="forgot-password/:token" element={<NewPassword />} />
         <Route path="*" element={<Error404 />} />
-        <Route path="search" element={<ComplexContainer/>} />
+        <Route path="search" element={<ComplexContainer />} />
         <Route path="search/:id" element={<ComplexDetails />} />
       </Route>
     </Routes>
