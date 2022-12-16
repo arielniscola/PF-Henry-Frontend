3a003897855525f32150671e17c0eada049a9831
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-15 21:12:44 -0300

    Added user related actions

--- my-app/src/redux/actionTypes.js
+++ my-app/src/redux/actionTypes.js
@@ -1,18 +1,23 @@
 //traer info
-export const GET_ALL_COMPLEX = "GET_ALL_COMPLEX"
-export const GET_COMPLEX_DETAIL = "GET_COMPLEX_DETAIL"
-export const GET_ALL_SPORTS = "GET_ALL_SPORTS"
-export const GET_ALL_SERVICES = "GET_ALL_SERVICES"
+export const GET_ALL_COMPLEX = "GET_ALL_COMPLEX";
+export const GET_COMPLEX_DETAIL = "GET_COMPLEX_DETAIL";
+export const GET_ALL_SPORTS = "GET_ALL_SPORTS";
+export const GET_ALL_SERVICES = "GET_ALL_SERVICES";
 
 //filters
-export const FILTER_BY_SERVICE = "FILTER_BY_SERVICE"
-export const FILTER_BY_SPORT = "FILTER_BY_SPORT"
-export const FILTER_BY_AZ = "FILTER_BY_AZ"
-export const FILTER_BY_FAV = "FILTER_BY_FAV"
+export const FILTER_BY_SERVICE = "FILTER_BY_SERVICE";
+export const FILTER_BY_SPORT = "FILTER_BY_SPORT";
+export const FILTER_BY_AZ = "FILTER_BY_AZ";
+export const FILTER_BY_FAV = "FILTER_BY_FAV";
 
 //search
-export const SEARCH_BY_CITY = "SEARCH_BY_CITY"
+export const SEARCH_BY_CITY = "SEARCH_BY_CITY";
 
 // cretae
-export const CREATE_USER = "CREATE_USER"
-export const CREATE_COMPLEX = "CREATE_COMPLEX"
+export const CREATE_USER = "CREATE_USER";
+export const CREATE_COMPLEX = "CREATE_COMPLEX";
+
+// user
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const CHECK_USER_SESSION = "CHECK_USER_SESSION";
