0dedd7ccd038733af955362da2421cdf07f5812d
Brian Leguizamon <104389659+Thundeck@users.noreply.github.com>
2023-01-02 18:36:22 -0300

    Update AboutUs.jsx

--- my-app/src/components/AboutUs.jsx
+++ my-app/src/components/AboutUs.jsx
@@ -1,49 +1,18 @@
-import React from "react";
-import DatePicker, {registerLocale} from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
-import en from 'date-fns/locale/es';
-registerLocale('en', en);
-
-
 const AboutUs = () => {
-  const [state, setState] = useState({
-    fecha: new Date()
-  });
-
-
-  const onChange = date => {
-    setState({
-      fecha: date
-    });
-    console.log("date",state.fecha);
-  }
-
-  return (
-    <div className="flex w-full h-32
-    items-center justify-center space-between
-    ">
-      <div >
-        <h1 className="text-2xl font-bold"
-        >Enter Date:</h1>
+  return <div className="flex flex-col h-full items-center justify-center">
+          <div className="self-center max-w-xl h-full">
+        <h2 className="flex flex-col items-center justify-center mb-5 text-4xl  font-bold text-blue-700">
+          About us
+        </h2>
+        <p className="flex flex-col items-center justify-center mt-10 text-2xl font-medium">
+          We show you the benefits of having an online channel to manage your
+          complex that allows users to book online.
+        </p>
+        <p className="flex flex-col items-center justify-center mt-10 text-2xl font-medium ">
+          Leave us your contact details so we can get in touch with you.
+        </p>
       </div>
-      <div className="flex
-        ml-4">
-        <DatePicker
-          
-          className="border border-gray-300 rounded-md p-2 "
-          onChange={onChange}
-          selected={state.fecha}
-          dateFormat="dd/MM/yyyy"
-          locale="en"
-          calendarClassName="bg-gray-200
-          border border-gray-300 rounded-md p-2 "
-        />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >Serch</button>
-      </div>
-    </div>
-  );
+  </div>;
 };
 
 export default AboutUs;
