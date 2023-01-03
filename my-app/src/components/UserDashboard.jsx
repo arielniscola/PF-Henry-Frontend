be8dc852d5b52233d9c9ba4edca9c30798c5d4d7
AlexisAraujo530 <alexisaraujo530@gmail.com>
2023-01-03 15:25:54 -0300

    estilo dashboard

--- my-app/src/components/UserDashboard.jsx
+++ my-app/src/components/UserDashboard.jsx
@@ -1,51 +1,44 @@
 import React from "react";
-import DatePicker, {registerLocale} from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
-import en from 'date-fns/locale/es';
-registerLocale('en', en);
+import { Link } from "react-router-dom";
+import perfil from "../data/perfil.jpeg";
+
 
 
 
 
 const UserDashboard = () => {
-  const [state, setState] = useState({
-    fecha: new Date()
-  });
 
-
-  const onChange = date => {
-    setState({
-      fecha: date
-    });
-    console.log("date",state.fecha);
-  }
 
   return (
-    <div className="flex w-full h-32
-    items-center justify-center space-between
-    ">
-      <div >
-        <h1 className="text-2xl font-bold"
-        >Enter Date:</h1>
-      </div>
-      <div className="flex
-        ml-4">
-        <DatePicker
-          
-          className="border border-gray-300 rounded-md p-2 "
-          onChange={onChange}
-          selected={state.fecha}
-          dateFormat="dd/MM/yyyy"
-          locale="en"
-          calendarClassName="bg-gray-200
-          border border-gray-300 rounded-md p-2 "
-        />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >Serch</button>
-      </div>
+    <div className="flex border border-gray-300 m-10 rounded-2xl p-2 "
+    style={{
+      height: "300px",
+      backgroundImage: `url(${perfil})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}
+    >
+     <div className="mt-10 ml-10">
+        <img src="https://i.ibb.co/0nQqZ3r/undraw-Online-chat-re-4r4y.png"
+        className="w-36 h-36 mb-3 border border-white rounded-md p-2" 
+        alt="undraw-Online-chat-re-4r4y" 
+        border="0" />
+        <h1 className="text-2xl font-bold mb-3 text-white"
+        >Nombre</h1>
+        </div>
+        <div className="mt-10 ml-10">
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-10"
+        >New Reservation</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10"
+        >My Reservations</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10"
+        >My Favorites</button>
+        <button className="bg-pink-400 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded ml-10"
+        >Edit Profile</button>
     </div>
-  );
+    </div>
+  )
 
 };
 
