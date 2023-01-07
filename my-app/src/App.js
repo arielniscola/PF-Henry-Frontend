6f256137374277764037d2b1a000af077a5bb6cb
Braian Gomez <braiandenisgomez@gmail.com>
2023-01-06 23:17:31 -0300

    added googleauthprovider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,14 +14,14 @@ import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
 import NewPassword from "./components/NewPassword";
 import { useEffect } from "react";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { checkUserSession } from "./redux/actions";
 import ConfirmAccount from "./components/ConfirmAccount";
 import ComplexForm from "./components/complexform";
 import Reservations from "./components/Reservations";
 import CreateCourt from "./components/CreateCourt";
 import ComplejoDisplayer from "./components/ComplejoDisplayer";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   const dispatch = useDispatch();
@@ -31,28 +31,29 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<LayOut />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<AboutUs />} />
-        <Route path="contact-us" element={<ContactUs />} />
-        <Route path="account" element={<Account />} />
-        <Route path="favorites" element={<ComplexFavorite />} />
-        <Route path="*" element={<Error404 />} />
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
-        <Route path="forgot-password" element={<ForgotPassword />} />
-        <Route path="forgot-password/:token" element={<NewPassword />} />
-        <Route path="confirm-account/:token" element={<ConfirmAccount />} />
-        <Route path="search" element={<ComplexContainer/>} />
-        <Route path="create" element={<ComplexForm/>} />
-        <Route path="search/:id" element={<ComplexDetails />} />
-        <Route path="reservations" element={<Reservations />} />
-        <Route path="complejodisplayer" element={<ComplejoDisplayer />} />
-        <Route path="createcourt" element={<CreateCourt />} />
- 
-      </Route>
-    </Routes>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_KEY}>
+      <Routes>
+        <Route path="/" element={<LayOut />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="contact-us" element={<ContactUs />} />
+          <Route path="account" element={<Account />} />
+          <Route path="favorites" element={<ComplexFavorite />} />
+          <Route path="*" element={<Error404 />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="forgot-password/:token" element={<NewPassword />} />
+          <Route path="confirm-account/:token" element={<ConfirmAccount />} />
+          <Route path="search" element={<ComplexContainer />} />
+          <Route path="create" element={<ComplexForm />} />
+          <Route path="search/:id" element={<ComplexDetails />} />
+          <Route path="reservations" element={<Reservations />} />
+          <Route path="complejodisplayer" element={<ComplejoDisplayer />} />
+          <Route path="createcourt" element={<CreateCourt />} />
+        </Route>
+      </Routes>
+    </GoogleOAuthProvider>
   );
 }
 
