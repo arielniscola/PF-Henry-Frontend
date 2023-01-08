dbf9205037a0d3f7e8a562aebefc21723654577b
Brian Leguizamon <104389659+Thundeck@users.noreply.github.com>
2023-01-08 16:56:36 -0300

    fix about us responsive

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -164,7 +164,7 @@ const NavBar = () => {
           </Link>
           <Link
             className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:text-gray-800 dark:hover:text-white"
-            to="/about-us"
+            to="/about"
           >
             About us
           </Link>
