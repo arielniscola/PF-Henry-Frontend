c53e13d7858bf20d7ce53fa9c3892d66020b3216
arielniscola <arielniscola@gmail.com>
2023-01-11 09:09:53 -0300

    alerts paymente reserves

--- my-app/src/components/ReservationCourt.jsx
+++ my-app/src/components/ReservationCourt.jsx
@@ -3,6 +3,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 const ReservationCourt = ({ court }) => {
   const [startDate, setStartDate] = useState(new Date());
@@ -34,10 +35,6 @@ const ReservationCourt = ({ court }) => {
       fechaHora.setHours(fechaHora.getHours() + 1, fechaHora.getMinutes() + 30);
       let addFecha = new Date(fechaHora);
       console.log(addFecha.toTimeString().substring(0, 8));
-      //   turns.forEach((turn) => {
-      //     console.log(addFecha.toTimeString().substring(0, 8));
-      //     console.log(turn.time_start);
-      //   });
       const filterTurns = turnsRes.find(
         (elem) => addFecha.toTimeString().substring(0, 8) === elem.time_start
       );
@@ -97,83 +94,99 @@ const ReservationCourt = ({ court }) => {
 
     console.log(result);
   };
-
+  const showAlertReservation = (time) => {
+    Swal.fire({
+      title: "Would you like to make a reservation?",
+      text: "Payment: Mercadopago",
+      icon: "info",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, I would like",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        saveTurn(time);
+        payment();
+      }
+    });
+  };
   return (
-      <div className="flex flex-col items-center m-12 h-72 border border-black 
+    <div
+      className="flex flex-col items-center m-12 h-72 border border-black 
       rounded-lg shadow-lg bg-gray-100
-      ">
-        <h2 className="flex flex-col items-center justify-center mt-10 text-4xl  font-bold text-blue-700">
+      "
+    >
+      <h2 className="flex flex-col items-center justify-center mt-10 text-4xl  font-bold text-blue-700">
         Shift Detail
-        </h2>
-        <div className="flex flex-row items-center text-2xl font-medium ml-60 ">
-          <div >
-          Pick date Turn:
-          </div>
-          <div>
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => selectedDate(date)}
-          className=" bg-white rounded-lg border border-gray-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none text-base px-2 m-5"
-        /></div>
-        </div>
-        <div className="flex flex-col items-center justify-center  text-2xl font-medium">
-        Available times:</div>
+      </h2>
+      <div className="flex flex-row items-center text-2xl font-medium ml-60 ">
+        <div>Pick date Turn:</div>
         <div>
-          <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
-            {times.length > 0 &&
-              times.map((time) => (
-                <li className="pb-3 sm:pb-4" key={time.time}>
-                  <div className="flex items-center space-x-4">
-                    <div className="flex-shrink-0"></div>
-                    <div className="flex-1 min-w-0">
-                      <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                        <b>{time.time}</b>
-                      </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                      {time.available ? (
-                        <span className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
-                          Available
-                        </span>
-                      ) : (
-                        <span className="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">
-                          No Available
-                        </span>
-                      )}
-                    </div>
-                    {time.available && (
-                      <div>
-                        <button
-                          type="button"
-                          className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
-                          onClick={() => {
-                            saveTurn(time.time);
-                            payment();
-                          }}
-                        >
-                          <svg
-                            aria-hidden="true"
-                            class="w-5 h-5"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                              clip-rule="evenodd"
-                            ></path>
-                          </svg>
-                          <span class="sr-only">Icon description</span>
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                </li>
-              ))}
-          </ul>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => selectedDate(date)}
+            className=" bg-white rounded-lg border border-gray-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none text-base px-2 m-5"
+          />
         </div>
       </div>
+      <div className="flex flex-col items-center justify-center  text-2xl font-medium">
+        Available times:
+      </div>
+      <div>
+        <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+          {times.length > 0 &&
+            times.map((time) => (
+              <li className="pb-3 sm:pb-4" key={time.time}>
+                <div className="flex items-center space-x-4">
+                  <div className="flex-shrink-0"></div>
+                  <div className="flex-1 min-w-0">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      <b>{time.time}</b>
+                    </p>
+                  </div>
+                  <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                    {time.available ? (
+                      <span className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                        Available
+                      </span>
+                    ) : (
+                      <span className="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">
+                        No Available
+                      </span>
+                    )}
+                  </div>
+                  {time.available && (
+                    <div>
+                      <button
+                        type="button"
+                        className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                        onClick={() => {
+                          showAlertReservation(time.time);
+                        }}
+                      >
+                        <svg
+                          aria-hidden="true"
+                          class="w-5 h-5"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                        <span class="sr-only">Icon description</span>
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
--- my-app/src/components/Reservations.jsx
+++ my-app/src/components/Reservations.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
+import Swal from "sweetalert2";
 
+const Reservations = (props) => {
+  const location = useLocation();
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const singleValue = queryParams.get("status");
+    showAlert(singleValue);
+  });
 
+  //showAlert(statusOperation);
 
-const Reservations = () => {
-    return <div>Reservations</div>;
-     
+  const showAlert = (status) => {
+    if (status === "approved") {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Payment successfully completed",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Payment Error",
+        text: "Something went wrong in payment method",
+      });
+    }
+  };
+  return <div>Reservations</div>;
 };
 
 export default Reservations;
-
-
